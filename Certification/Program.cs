8c8c3874d753219134a23f0c710c1c22650bb971
Jairo Roberto Gómez Alvarez <jgomez@sitek.mx>
2017-07-16 23:49:16 -0600

    First Lines

--- Certification/Program.cs
+++ Certification/Program.cs
@@ -1,16 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
-namespace Certification
+namespace Chapter1
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void ThreadMethod()
         {
-            Console.WriteLine("Message");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("ThreadProc: {0}", i);
+                Thread.Sleep(0);
+            }
+        }
+
+        public static void Main()
+        {
+            Thread t = new Thread(new ThreadStart(ThreadMethod));
+            t.Start();
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Main thread: Do some work.");
+                Thread.Sleep(0);
+            }
+
+            t.Join();
         }
     }
 }
